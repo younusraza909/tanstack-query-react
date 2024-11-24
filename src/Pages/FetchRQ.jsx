1caafccd1a9a8c7f87eb63946ed1be184222ad45
younusraza909 <younusraza909@gmail.com>
2024-11-24 15:49:03 +0500

    Pagination in react query

--- src/Pages/FetchRQ.jsx
+++ src/Pages/FetchRQ.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { fetchPosts } from "../API/api";
-import { useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 
 export const FetchRQ = () => {
+  const [pageNumber, setPageNumber] = useState(0);
+
   const { data, isPending, isError, error } = useQuery({
-    queryKey: ["posts"],
-    queryFn: fetchPosts,
+    queryKey: ["posts", pageNumber],
+    queryFn: () => fetchPosts(pageNumber),
+    placeholderData: keepPreviousData, // when next data is fetched it wil show previous data instead of loading
     // refetchInterval:1000, // For Polling
     // refetchIntervalInBackground: true, // Polling in background as well else it will not poll when we are out of scope
   });
@@ -29,6 +33,17 @@ export const FetchRQ = () => {
           );
         })}
       </ul>
+
+      <div className="pagination-section container">
+        <button
+          disabled={pageNumber === 0 ? true : false}
+          onClick={() => setPageNumber((prev) => prev - 3)}
+        >
+          Prev
+        </button>
+        <h2>{pageNumber / 3 + 1}</h2>
+        <button onClick={() => setPageNumber((prev) => prev + 3)}>Next</button>
+      </div>
     </div>
   );
 };
